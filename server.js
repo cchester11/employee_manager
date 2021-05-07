fd42dcfaeb5f7a9b27c816719664b4c54532269b
Charles <cchester1@angelo.edu>
2021-05-07 18:13:04 -0500

    basic get routes created

--- server.js
+++ server.js
@@ -1,42 +1,95 @@
-const express = require('express');
-const mysql = require('mysql2');
+const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 7543;
 const app = express();
 
 // Middleware
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // connect to the database
-const database = mysql.createConnection(
-  {
-  host: 'localhost',
-  user: 'root',
-  database: 'employee_manager_db'
-  },
-  console.log('Connected to the database')
-);
+const database = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "employee_manager_db",
+});
 
-// get and post routes with db query functionality
-app.get('/api/employeeManager', (req, res) => {
-  res.json({
-    message: 'connected',
-  })
-})
+// get request
+app.get("/api/departments", (req, res) => {
+  const sql = `SELECT * FROM department`;
 
-app.post('/api/employeeManager', (req, res) => {
-  res.json({
-    message: 'success'
-  })
-})
+  database.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+app.get("/api/roles", (req, res) => {
+  const sql = `SELECT * FROM employee_role`;
+
+  database.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+app.get("/api/employees", (req, res) => {
+  const sql = `SELECT * FROM employee`;
+
+  database.query(sql, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: result,
+    });
+  });
+});
+
+// post request
+app.post("/api/employeemanager", ({ body }, res) => {
+  const sql = `INSERT INTO employee (first_name, last_name, emp_role_id) 
+  VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.emp_role_id];
+
+  database.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "succes",
+      data: body,
+      changes: result.affectedRows,
+    });
+  });
+});
 
 // Default for error in request
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 // Server ago
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`)
+database.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
