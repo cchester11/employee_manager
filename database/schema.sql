a4eb0fc96bf4cdd371e4229cdf25e76a79f099fc
Charles <cchester1@angelo.edu>
2021-05-07 12:06:35 -0500

    database relations secured

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS employee_role;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS employee_role;
+DROP TABLE IF EXISTS department;
 
 CREATE TABLE department(
   id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
@@ -10,13 +10,14 @@ CREATE TABLE employee_role(
   id INTEGER PRIMARY KEY AUTO_INCREMENT, 
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INTEGER NOT NULL 
+  department_id INTEGER ,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee(
   id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INTEGER,
-  manager_id INTEGER  
+  emp_role_id INTEGER,
+  CONSTRAINT role_key FOREIGN KEY (emp_role_id) REFERENCES employee_role(id) ON DELETE SET NULL   
 );
--- database/seeds.sql
+++ database/seeds.sql
@@ -1,17 +1,17 @@
-INSERT INTO department (id, department_name)
+INSERT INTO department (department_name)
 VALUES
-  (1, 'Supplychain_management'),
-  (2, 'Finances'),
-  (3, 'Sales/Employee');
+  ('Supplychain_management'),
+  ('Finances'),
+  ('Sales/Employee');
 
-INSERT INTO employee_role (id, title, salary, department_id)
+INSERT INTO employee_role (title, salary, department_id)
 VALUES
-  (1,'Supplychain_management', 100000.00, 1),
-  (2, 'Finances', 100000.00, 2),
-  (3, 'Sales/Employee', 50000.00, 3);
+  ('Supplychain_management', 100000.00, 1),
+  ('Finances', 100000.00, 2),
+  ('Sales/Employee', 50000.00, 3);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, emp_role_id)
 VALUES
-  (1, 'Ray', 'Lewis', 1, 1),
-  (2, 'Lesley', 'Smith', 'Supervision', 2, 2),
-  (3, 'Mario', 'Super', 'Sales/Employee', 3, 3);
+  ('Ray', 'Lewis', 1),
+  ('Lesley', 'Smith', 2),
+  ('Mario', 'Super', 3);
